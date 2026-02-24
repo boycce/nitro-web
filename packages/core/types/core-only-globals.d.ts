6c2a2f0bcbd43bffaaee43b95c15f0a77ff16c0f
Ricky Boyce <boycce@gmail.com>
2026-02-24 16:13:07 +1000

    remove ./core/types

--- packages/core/types/core-only-globals.d.ts
+++
@@ -1,9 +0,0 @@
-import { Store } from 'nitro-web/types'
-import { Dispatch, SetStateAction } from 'react'
-
-declare global {
-  // useTracked global (normally defined in your project's /client/index.ts)
-  const useTracked: () => [Store, Dispatch<SetStateAction<Store>>]
-}
-
-export {}
