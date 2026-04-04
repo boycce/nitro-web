f22c3ce7525ddc3ebcc242b01ad4e635b03c077f
Ricky Boyce <boycce@gmail.com>
2026-04-04 11:23:32 +1000

    improved initials

--- packages/core/components/partials/element/initials.tsx
+++ packages/core/components/partials/element/initials.tsx
@@ -1,4 +1,4 @@
-import { css } from 'twin.macro'
+import { twMerge } from 'nitro-web/util'
 
 type InitialsProps = {
   icon?: { initials: string, hex: string }
@@ -12,55 +12,18 @@ type InitialsProps = {
 export function Initials({ icon, isBig, isMedium, isSmall, isRound, className }: InitialsProps) {
   return (
     <span 
-      css={style} 
-      class={
-        'initials-square' +
-        (isBig ? ' is-big' : '') + 
-        (isMedium ? ' is-medium' : '') + 
-        (isSmall ? ' is-small' : '') + 
-        (isRound ? ' is-round' : '') + 
-        (icon ? '' : ' is-empty') + 
-        ' nitro-initials' +
-        (className ? ' ' + className : '')
-      }
+      className={twMerge(
+        'nitro-initials flex items-center justify-center rounded-[5px] font-bold text-[11px] size-[24px]',
+        isBig && 'size-[40px] text-sm',
+        isMedium && 'size-[30px] text-xs',
+        isSmall && 'size-[22px]',
+        isRound && 'rounded-full',
+        !icon && 'w-0',
+        className
+      )}
       style={icon ? {backgroundColor: icon?.hex + '15', color: icon?.hex} : {}}
     >
       {icon?.initials}
     </span>
   )
 }
-
-const style = css`
-  // seen in input.jsx
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  border-radius: 5px;
-  font-weight: 700;
-  font-size: 11px;
-  width: 24px;
-  height: 24px;
-  // new
-  &.is-medium {
-    width: 30px;
-    height: 30px;
-    font-size: 12px;
-  }
-  // seen in select.jsx
-  &.is-small {
-    width: 22px;
-    height: 22px;
-    font-size: 11px;
-  }
-  &.is-big {
-    width: 48px;
-    height: 48px;
-    font-size: 14px;
-  }
-  &.is-round {
-    border-radius: 50%;
-  }
-  &.is-empty {
-    width: 0;
-  }
-`
